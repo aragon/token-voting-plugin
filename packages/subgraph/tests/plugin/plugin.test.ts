bbb278cfab4aa356c2eb58d7e3bdef7447f92480
Claudia <claudiabarcelovaldes40@gmail.com>
2024-04-05 16:03:55 +0200

    feat: add checks for the template creation in the membership contract announced handler test, remove action test because it is already being checked in the handleProposalCreated test

--- packages/subgraph/tests/plugin/plugin.test.ts
+++ packages/subgraph/tests/plugin/plugin.test.ts
@@ -34,20 +34,13 @@ import {
 } from '@aragon/osx-commons-subgraph';
 import {Address, bigInt, BigInt, ethereum} from '@graphprotocol/graph-ts';
 import {
+  afterAll,
   assert,
   clearStore,
   describe,
   newMockEvent,
   test,
 } from 'matchstick-as/assembly/index';
-
-let dummyActionTo = DAO_TOKEN_ADDRESS;
-let dummyActionValue = '0';
-let dummyActionData = '0x00000000';
-
-let actions = [
-  createDummyAction(dummyActionTo, dummyActionValue, dummyActionData),
-];
 
 test('Run TokenVoting (handleProposalCreated) mappings with mock event', () => {
   // check store is empty before running the test
@@ -224,9 +217,10 @@ test('Run TokenVoting (handleVoteCast) mappings with mock event', () => {
 
 test('Run TokenVoting (handleVoteCast) mappings with mock event and vote option "None"', () => {
   let proposal = new ExtendedTokenVotingProposal().withDefaultValues();
+  let action = new ExtendedAction().withDefaultValues();
 
   // create calls
-  proposal.mockCall_getProposal(actions);
+  proposal.mockCall_getProposal([action.getDummyAction()]);
 
   // create event
   let voter = new ExtendedTokenVotingVoter().withDefaultValues();
@@ -293,7 +287,13 @@ test('Run TokenVoting (handleVotingSettingsUpdated) mappings with mock event', (
 });
 
 describe('handleMembershipContractAnnounced', () => {
+  afterAll(() => {
+    clearStore();
+  });
+
   test('it should create an erc20 and assign its address to the tokenVotingPlugin', () => {
+    assert.dataSourceCount('GovernanceERC20', 0);
+
     // create entities
     let tokenVotingPlugin = new ExtendedTokenVotingPlugin().withDefaultValues();
     let erc20Contract = new ExtendedERC20Contract().withDefaultValues();
@@ -317,10 +317,14 @@ describe('handleMembershipContractAnnounced', () => {
     tokenVotingPlugin.assertEntity();
     erc20Contract.assertEntity();
 
-    clearStore();
+    // check the template were created
+    assert.dataSourceCount('GovernanceERC20', 1);
+    assert.dataSourceExists('GovernanceERC20', erc20Contract.id);
   });
 
   test('it should create an erc20Wrapped and assign an erc20 as the underlying token and assign the erc20Wrapped address to the tokenVotingPlugin', () => {
+    assert.dataSourceCount('GovernanceERC20', 0);
+
     // create entities
     let tokenVotingPlugin = new ExtendedTokenVotingPlugin().withDefaultValues();
     let erc20Contract = new ExtendedERC20Contract().withDefaultValues();
@@ -359,100 +363,8 @@ describe('handleMembershipContractAnnounced', () => {
     erc20Contract.assertEntity();
     erc20WrappedContract.assertEntity();
 
-    clearStore();
-  });
-});
-
-describe('Testing Actions', () => {
-  test('A new proposal action is registered during the proposal creation', () => {
-    // manual re-write so this approach can be ported to other plugins
-    assert.entityCount('Action', 0);
-    assert.entityCount('TokenVotingProposal', 0);
-
-    let tokenVotingPlugin = new ExtendedTokenVotingPlugin().withDefaultValues();
-    let proposal = new ExtendedTokenVotingProposal().withDefaultValues();
-
-    // step 1: create the mock proposal event
-    tokenVotingPlugin.proposalCount = BigInt.fromString(ONE);
-    tokenVotingPlugin.mockCall_getProposalCountCall();
-    proposal.mockCall_getProposal(actions);
-    proposal.mockCall_totalVotingPower();
-
-    const proposalEvent = changetype<ProposalCreated>(newMockEvent());
-    proposalEvent.address = Address.fromString(proposal.plugin);
-
-    let proposalId = new BigInt(0);
-    let creator = proposal.creator;
-    let startDate = proposal.startDate;
-    let endDate = proposal.endDate;
-
-    let metadata: string;
-    if (proposal.metadata) {
-      metadata = proposal.metadata as string;
-    } else {
-      metadata = 'metadata';
-    }
-    let allowFailureMap = proposal.allowFailureMap;
-
-    let proposalIdEvent = new ethereum.EventParam(
-      'proposalId',
-      ethereum.Value.fromUnsignedBigInt(
-        BigInt.fromString(proposalId.toString())
-      )
-    );
-
-    let creatorEvent = new ethereum.EventParam(
-      'creator',
-      ethereum.Value.fromAddress(Address.fromBytes(creator))
-    );
-    let startDateEvent = new ethereum.EventParam(
-      'startDate',
-      ethereum.Value.fromUnsignedBigInt(startDate)
-    );
-    let endDateEvent = new ethereum.EventParam(
-      'endDate',
-      ethereum.Value.fromUnsignedBigInt(endDate)
-    );
-    let metadataEvent = new ethereum.EventParam(
-      'metadata',
-      ethereum.Value.fromString(metadata as string)
-    );
-    let actionsEvent = new ethereum.EventParam(
-      'actions',
-      ethereum.Value.fromTupleArray(actions)
-    );
-    let allowFailureMapEvent = new ethereum.EventParam(
-      'allowFailureMap',
-      ethereum.Value.fromUnsignedBigInt(allowFailureMap)
-    );
-
-    proposalEvent.parameters = [
-      proposalIdEvent,
-      creatorEvent,
-      startDateEvent,
-      endDateEvent,
-      metadataEvent,
-      actionsEvent,
-      allowFailureMapEvent,
-    ];
-
-    // step 2: handle the proposal event
-    _handleProposalCreated(proposalEvent, DAO_ADDRESS, metadata);
-
-    // step 3: check that the proposal action was created
-    assert.entityCount('Action', 1);
-    assert.entityCount('TokenVotingProposal', 1);
-
-    // step 3.1: check that the action has the correct fields
-    const actionID = generateActionEntityId(
-      Address.fromString(proposal.plugin),
-      Address.fromString(DAO_ADDRESS),
-      proposalId.toString(),
-      0
-    );
-    assert.fieldEquals('Action', actionID, 'to', dummyActionTo.toLowerCase());
-    assert.fieldEquals('Action', actionID, 'value', dummyActionValue);
-    assert.fieldEquals('Action', actionID, 'data', dummyActionData);
-    assert.fieldEquals('Action', actionID, 'proposal', proposal.id);
+    // check the template were created
+    assert.dataSourceCount('GovernanceERC20', 1);
+    assert.dataSourceExists('GovernanceERC20', erc20Contract.id);
   });
 });
