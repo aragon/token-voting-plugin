4bff3649d64c1bf97f97460a17535503f65c9e19
Michael Heuer <michael.heuer@rwth-aachen.de>
2024-04-08 18:16:29 +0200

    fix: deleted redundant import to satisfy linter

--- packages/contracts/test/10_unit-testing/token-voting/11_token-voting.ts
+++ packages/contracts/test/10_unit-testing/token-voting/11_token-voting.ts
@@ -54,7 +54,6 @@ import {loadFixture, time} from '@nomicfoundation/hardhat-network-helpers';
 import {SignerWithAddress} from '@nomiclabs/hardhat-ethers/signers';
 import {expect} from 'chai';
 import {BigNumber} from 'ethers';
-import {deepCopy} from 'ethers/lib/utils';
 import {ethers} from 'hardhat';
 
 type GlobalFixtureResult = {
