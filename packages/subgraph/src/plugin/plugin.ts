c7dd2b72e628c80d7dce3ac83185ea1f7008d544
Claudia <claudiabarcelovaldes40@gmail.com>
2024-04-12 15:10:23 +0200

    fix: define the received index, not hte fixed one

--- packages/subgraph/src/plugin/plugin.ts
+++ packages/subgraph/src/plugin/plugin.ts
@@ -76,11 +76,11 @@ export function _handleProposalCreated(
     // ProposalParameters
     const parameters = proposal.value.value2;
     let votingModeIndex = parameters.votingMode;
+    proposalEntity.votingModeIndex = votingModeIndex;
     if (!VOTING_MODES.has(votingModeIndex)) {
       // if the voting mode is not defined, set it to 'Undefined'
       votingModeIndex = VOTING_MODE_UNDEFINED;
     }
-    proposalEntity.votingModeIndex = votingModeIndex;
     proposalEntity.votingMode = VOTING_MODES.get(votingModeIndex) as string;
     proposalEntity.supportThreshold = parameters.supportThreshold;
     proposalEntity.snapshotBlock = parameters.snapshotBlock;
@@ -274,12 +274,12 @@ export function handleVotingSettingsUpdated(
   );
   if (pluginEntity) {
     let votingModeIndex = event.params.votingMode;
+    pluginEntity.votingModeIndex = votingModeIndex;
     if (!VOTING_MODES.has(votingModeIndex)) {
       // if the voting mode is not defined, set it to 'Undefined'
       votingModeIndex = VOTING_MODE_UNDEFINED;
     }
 
-    pluginEntity.votingModeIndex = votingModeIndex;
     pluginEntity.votingMode = VOTING_MODES.get(votingModeIndex) as string;
     pluginEntity.supportThreshold = event.params.supportThreshold;
     pluginEntity.minParticipation = event.params.minParticipation;
