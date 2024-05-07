2e88af75aa445bb990b131fe259ea8a33e5c91e9
Claudia <claudiabarcelovaldes40@gmail.com>
2024-05-07 16:43:41 +0200

    ci: pr review comment

--- packages/contracts/test/10_unit-testing/12_plugin-setup.ts
+++ packages/contracts/test/10_unit-testing/12_plugin-setup.ts
@@ -579,7 +579,6 @@ describe('TokenVotingSetup', function () {
       const plugin = ethers.Wallet.createRandom().address;
 
       // Make a static call to check that the plugin update data being returned is correct.
-      // const prepareUpdateData
       const {
         initData: initData,
         preparedSetupData: {helpers, permissions},
