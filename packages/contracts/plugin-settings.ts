d3d51b8dd7286f09a60ea44dbcd9dccf222c2664
Jordaniza <jordan.imran@predixa.ai>
2024-06-11 18:36:33 +0200

    f: added skip logic to setup contract and fixed most tests, 2 still failing

--- packages/contracts/plugin-settings.ts
+++ packages/contracts/plugin-settings.ts
@@ -1,7 +1,6 @@
 import buildMetadata from './src/build-metadata.json';
 import releaseMetadata from './src/release-metadata.json';
 import {GovernanceERC20} from './test/test-utils/typechain-versions';
-import {VersionTag} from '@aragon/osx-commons-sdk';
 import {ethers} from 'hardhat';
 
 export const PLUGIN_CONTRACT_NAME = 'TokenVoting'; // This must match the filename `packages/contracts/src/MyPlugin.sol` and the contract name `MyPlugin` within.
@@ -11,9 +10,9 @@ export const PLUGIN_REPO_ENS_SUBDOMAIN_NAME = 'token-voting'; // This will resul
 export const GOVERNANCE_ERC20_CONTRACT_NAME = 'GovernanceERC20';
 export const GOVERNANCE_WRAPPED_ERC20_CONTRACT_NAME = 'GovernanceWrappedERC20';
 
-export const VERSION: VersionTag = {
-  release: 1, // Increment this number ONLY if breaking/incompatible changes were made. Updates between releases are NOT possible.
-  build: 3, // Increment this number if non-breaking/compatible changes were made. Updates to newer builds are possible.
+export const VERSION = {
+  release: 2, // Increment this number ONLY if breaking/incompatible changes were made. Updates between releases are NOT possible.
+  build: 0, // Increment this number if non-breaking/compatible changes were made. Updates to newer builds are possible.
 };
 
 // The metadata associated with the plugin version you are currently working on.
