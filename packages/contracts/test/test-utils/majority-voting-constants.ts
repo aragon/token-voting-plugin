b6e9ca777216f682cab5c811d855314de81930fe
Claudia <claudiabarcelovaldes40@gmail.com>
2024-08-28 14:51:47 +0200

    feat: define new interfaces to check them on the tests

--- packages/contracts/test/test-utils/majority-voting-constants.ts
+++ packages/contracts/test/test-utils/majority-voting-constants.ts
@@ -7,6 +7,17 @@ export const MAJORITY_VOTING_BASE_INTERFACE = new ethers.utils.Interface([
   'function totalVotingPower(uint256)',
   'function getProposal(uint256)',
   'function updateVotingSettings(tuple(uint8,uint32,uint32,uint64,uint256))',
+  'function updateMinApproval(uint32)',
+  'function createProposal(bytes,tuple(address,uint256,bytes)[],uint256,uint64,uint64,uint8,bool)',
+]);
+
+export const MAJORITY_VOTING_BASE_OLD_INTERFACE = new ethers.utils.Interface([
+  'function minDuration()',
+  'function minProposerVotingPower()',
+  'function votingMode()',
+  'function totalVotingPower(uint256)',
+  'function getProposal(uint256)',
+  'function updateVotingSettings(tuple(uint8,uint32,uint32,uint64,uint256))',
   'function createProposal(bytes,tuple(address,uint256,bytes)[],uint256,uint64,uint64,uint8,bool)',
 ]);
 
