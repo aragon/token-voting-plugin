5629156b1b0baa9036ff6757a5a3ad072d101638
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2025-01-19 13:03:17 +0400

    linter fix

--- packages/contracts/types/hardhat.d.ts
+++ packages/contracts/types/hardhat.d.ts
@@ -3,8 +3,15 @@
 import {Wrapper} from '../test/test-utils/wrapper';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 
+export type VerifyEntry = {
+  address: string;
+  args?: unknown[];
+};
+
 declare module 'hardhat/types/runtime' {
   interface HardhatRuntimeEnvironment {
     wrapper: Wrapper;
+    aragonToVerifyContracts: VerifyEntry[];
   }
 }
+
