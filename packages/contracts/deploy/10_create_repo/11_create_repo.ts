11c80d78953f8018f1632c370fd8fe16ad3657d6
Claudia <claudiabarcelovaldes40@gmail.com>
2025-01-30 17:38:17 +0100

    fix: rename the json contract key

--- packages/contracts/deploy/10_create_repo/11_create_repo.ts
+++ packages/contracts/deploy/10_create_repo/11_create_repo.ts
@@ -125,13 +125,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   saveToDeployedJson(
     [
       {
-        name: PLUGIN_CONTRACT_NAME + 'PluginRepo',
+        name: PLUGIN_CONTRACT_NAME + 'RepoProxy',
         address: pluginRepo.address,
         blockNumber: tx.blockNumber,
         txHash: tx.hash,
       },
       {
-        name: PLUGIN_CONTRACT_NAME + 'PluginRepoImplementation',
+        name: PLUGIN_CONTRACT_NAME + 'RepoImplementation',
         address: await pluginRepoFactory.pluginRepoBase(),
         blockNumber: null,
         txHash: null,
